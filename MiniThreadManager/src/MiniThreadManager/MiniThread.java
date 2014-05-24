cdbd1423649d67605859549fce01ebf0ac9b5b8c
Kevin Rossi <KevinRossi77@gmail.com>
2014-05-24 00:41:53 -0400

    Import from JHU Analysis App.

--- MiniThreadManager/src/MiniThreadManager/MiniThread.java
+++ MiniThreadManager/src/MiniThreadManager/MiniThread.java
@@ -8,5 +8,21 @@ public class MiniThread extends Thread {
 	public long startTime = 0;
 	public long killTime = 0;	
 	public long endTime = 0;
+	
+	public static String toCSVHeader() {
+		return "threadLabel,endMessage\n";
+	}
+	
+	public String toCSV() {
+		String out = "";
 
+		out += threadLabel;
+		out += ",";
+		out += endMessage;
+		out += ",";
+		
+		out += "\n";
+
+		return out;
+	}
 }
